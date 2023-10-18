faa6da19aa288fd0de66c300479fb37c1702bbc7
Chase <123278965+chaszm@users.noreply.github.com>
2023-10-18 16:03:05 -0400

    Add files via upload

---
+++ EchoClient.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.net.Socket;
+
+//client gets message from user and sends message to reverse echo server
+//when reversed message arrives to server it displays message to user
+
+//if user wants to stop client program, they type end.
+// this will make client send message to server, and then WAITS for the message "dne" from server
+// if client recieves dne message, terminates without showing dne
+
+
+public class EchoClient {
+    public static void main(String[] args){
+
+     Socket sock = null;
+     InputStreamReader input = null;
+     OutputStreamWriter output = null;
+
+
+     BufferedReader bufferR = null;
+        BufferedWriter bufferW = null;
+
+
+
+        try{
+            sock = new Socket("localhost", 5555);
+
+            input = new InputStreamReader(sock.getInputStream());
+            output = new OutputStreamWriter(sock.getOutputStream());
+
+            bufferR = new BufferedReader(input);
+            bufferW = new BufferedWriter(output);
+
+            Scanner sc = new Scanner(System.in);  //input from keyboard
+
+            while(true){  //always runs
+
+            String message = sc.nextLine();                 //SENDING MESSAGE TO SERVER
+            bufferW.write(message);
+            bufferW.newLine();
+            bufferW.flush(); // flush when enter key
+
+            message = bufferR.readLine();
+            System.out.println("Server: " + message); // message from SERVER (reverse)
+
+
+
+
+
+            if(message.equalsIgnoreCase("dne"))  //if message sent to server is DNE??, break out
+                break;
+
+            }
+        }
+
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally{
+            try{
+                if(sock != null)
+                    sock.close();
+                if(input != null)
+                    input.close();
+                if(output != null)
+                    output.close();
+                if (bufferR != null)
+                    bufferR.close();
+                if (bufferW != null)
+                    bufferW.close();
+            } catch (IOException e){
+
+                e.printStackTrace();
+
+            }
+        }
+
+
+    }
+
+}
---
+++ ReverseEchoServer.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class ReverseEchoServer {
+    public static void main(String[] args) throws IOException {
+
+        Socket sock = null;
+        InputStreamReader input = null;
+        OutputStreamWriter output = null;
+        BufferedReader bufferR = null;
+        BufferedWriter bufferW = null;
+
+        ServerSocket serverSocket = null;
+
+        serverSocket = new ServerSocket(5555); // port number of client must match
+
+        while(true){
+
+            try{
+               sock = serverSocket.accept();
+
+               input = new InputStreamReader(sock.getInputStream());
+               output = new OutputStreamWriter(sock.getOutputStream());
+
+               bufferR = new BufferedReader(input);
+               bufferW = new BufferedWriter(output);
+
+
+               while(true){
+
+                   String message = bufferR.readLine();
+
+                   System.out.println("client: " + message);
+
+                    char charIndex;
+                    String reverse = "";
+
+                   for (int i=0; i<message.length(); i++){  //reverses the message from client
+
+                       charIndex = message.charAt(i); //index i of the string
+                       reverse = charIndex+reverse;
+                   }
+
+
+
+
+
+                   bufferW.write(reverse); // make server send REVERSE message
+                   bufferW.newLine();
+                   bufferW.flush();
+
+
+
+
+
+                   if(reverse.equalsIgnoreCase("dne")) { //if message is dne, break out of while
+                       break;
+                   }
+               }
+
+                System.exit(0); //exit server
+
+
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+    }
+}
